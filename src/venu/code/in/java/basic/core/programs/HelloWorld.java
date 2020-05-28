825a5901b153c570f13f610df2dc481dec74395d
venukuma <maddelavenukumar@gmail.comgit config --global user.name Your>
2020-05-28 11:47:06 +0530

    First Commit

---
+++ src/venu/code/in/java/basic/core/programs/HelloWorld.java
@@ -0,0 +1,12 @@
+package venu.code.in.java.basic.core.programs;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		//Testing Purpose
+		System.out.println("Hello! World");
+
+	}
+
+}
